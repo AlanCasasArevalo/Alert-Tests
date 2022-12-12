9395cf3d14a02b12dda91d60dd9e98cc45c2a9b3
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-12-12 13:16:54 +0100

    Alert was added to viewController

--- Alert/Views/ViewController.swift
+++ Alert/Views/ViewController.swift
@@ -1,9 +1,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private(set) var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction private func buttonTap() {
+        let alert = UIAlertController(title: "Do the thing?", message: "Let us know if you want to do the thing.", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            print("OK")
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        alert.preferredAction = okAction
+        
+        present(alert, animated: true)
     }
 }
 
